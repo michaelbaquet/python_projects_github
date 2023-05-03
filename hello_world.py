494c0fb2ab80a40af4bc791d580a156a9f6df397
Jason Michael <jmichaelbaquet@protonmail.com>
2023-05-02 23:04:08 -0600

    first commit with test

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# hello_world.py
+
+def say_hello():
+    print("Helloew WOrld")
+    return "Hello Worldz"
+
---
+++ test_hello_world.py
@@ -0,0 +1,13 @@
+# test hellow_world_test.py
+
+import unittest
+from hello_world import say_hello
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_say_hello(self):
+        self.assertEqual(say_hello(), "Hello Worldz")
+
+
+if __name__ == '__main__':
+    unittest.main()

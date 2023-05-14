7ad41745c24cfc522e5e3811c13201bc8f292290
Jason Michael <jmichaelbaquet@protonmail.com>
2023-05-14 15:33:26 -0600

    Add ReLU activation function and data generation

--- NEURAL_NETWORKS_FROM_SCRATH/neural_network.py
+++ NEURAL_NETWORKS_FROM_SCRATH/neural_network.py
@@ -1,10 +1,13 @@
 import numpy as np
+import spiral_data
 
 np.random.seed(0)
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+# X = [[1, 2, 3, 2.5],
+#      [2.0, 5.0, -1.0, 2.0],
+#      [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data.spiral_data(100,3)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -13,10 +16,19 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
 layer1.forward(X)
+print(layer1.output)
 
-layer2 = Layer_Dense(5, 2)
-layer2.forward(layer1.output)
+print("FIRST FIRST FIRST FIRST FIRST")
+print("AFTER AFTER AFTER AFTER")
 
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)

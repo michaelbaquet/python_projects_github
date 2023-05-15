007fa723f341e68912c76cfa28a36e1216c63e3a
Jason Michael <jmichaelbaquet@protonmail.com>
2023-05-15 13:07:09 -0600

    Add Softmax activation function

--- NEURAL_NETWORKS_FROM_SCRATH/neural_network.py
+++ NEURAL_NETWORKS_FROM_SCRATH/neural_network.py
@@ -1,13 +1,7 @@
 import numpy as np
 import spiral_data
 
-np.random.seed(0)
-
-# X = [[1, 2, 3, 2.5],
-#      [2.0, 5.0, -1.0, 2.0],
-#      [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data.spiral_data(100,3)
+# np.random.seed(0)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -16,19 +10,28 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2, 5)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+X, y = spiral_data.spiral_data(100, 3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-print(layer1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
 
-print("FIRST FIRST FIRST FIRST FIRST")
-print("AFTER AFTER AFTER AFTER")
+dense1.forward(X)
+activation1.forward(dense1.output)
 
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])

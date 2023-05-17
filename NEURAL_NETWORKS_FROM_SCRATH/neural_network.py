149bb6271bfd9305b1c48c610daa05a8dcf230ca
Jason Michael <jmichaelbaquet@protonmail.com>
2023-05-17 16:47:50 -0600

    Add Categorical cross-entropy loss function

--- NEURAL_NETWORKS_FROM_SCRATH/neural_network.py
+++ NEURAL_NETWORKS_FROM_SCRATH/neural_network.py
@@ -20,7 +20,30 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):   ### output is output from the model, y is intended target values
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_prediction, y_true):
+        samples = len(y_prediction)
+        y_pred_clipped = np.clip(y_prediction, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true] 
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        else:
+            raise ValueError("expected 1D or 2D array but got {}".format(y_true.shape))
+        
+        negative_log_liklihood = -np.log(correct_confidences)
+        return negative_log_liklihood
+
+
 X, y = spiral_data.spiral_data(100, 3)
+
 
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
@@ -29,9 +52,17 @@ dense2 = Layer_Dense(3, 3)
 activation2 = Activation_Softmax()
 
 dense1.forward(X)
+print(dense1.output[:5])
 activation1.forward(dense1.output)
+print(activation1.output[:5])
 
 dense2.forward(activation1.output)
+print(dense2.output[:5])
 activation2.forward(dense2.output)
-
 print(activation2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("at loss")
+print(loss)
